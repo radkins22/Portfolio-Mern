657d100f2a7ac7a309ef0e61d5486de47b8457ad
Rachael <rma199022@gmail.com>
2025-03-27 09:56:12 -0500

    finished experience page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,11 @@ import Nav from "./Views/Components/Nav/Nav";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Views/Pages/Home/Home";
 import About from "./Views/Pages/About/About";
+import Experience from "./Views/Pages/Experience/Experience";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Skills from "./Views/Pages/Skills/Skills";
+// import Projects from "./Views/Pages/Projects/Projects";
+// import Contact from "./Views/Pages/Contact/Contact";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -12,6 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        {/* <Route path="/skills" element={<Skills />} /> */}
         {/* <Route path="/projects" element={<Projects />} /> */}
         {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
